27d8671eea3378d1090b5bdf24b02d4d646abb2b
happosade <ilari.oras@kapsi.fi>
2021-11-29 16:43:56 +0200

    Services starting

--- kea-ca/entrypoint.sh
+++ kea-ca/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+sleep 30;
 /usr/sbin/kea-dhcp4 -c /kea-ctrl/kea-dhcp4.json &
 /usr/sbin/kea-ctrl-agent -c /kea-ctrl/kea-ctrl-agent.json
--- kea-dhcp4/entrypoint.sh
+++ kea-dhcp4/entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sleep 30;
 /usr/sbin/kea-dhcp4 -c /kea-dhcp/kea-dhcp4.json
